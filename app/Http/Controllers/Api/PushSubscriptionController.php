1c3873505c24d6c3d9574db73a4ab37be39d672d
YeMyintSoeKMD <yms.yemyintsoe.kmd@gmail.com>
2024-08-29 00:04:06 +0630

    push noti latest

--- app/Http/Controllers/Api/PushSubscriptionController.php
+++ app/Http/Controllers/Api/PushSubscriptionController.php
@@ -5,22 +5,33 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\PushSubscription;
 use Illuminate\Http\Request;
+use Exception;
+use Illuminate\Support\Facades\DB;
 use Minishlink\WebPush\Subscription;
 use Minishlink\WebPush\WebPush;
 
 class PushSubscriptionController extends Controller
 {
     /**
-     * Subscription by clients
+     * Subscribed by clients
      */
     public function pushSubscribe(Request $request)
     {
+        $doesAlreadyExist = PushSubscription::where('data', $request->getContent())->first();
+
+        if ($doesAlreadyExist) {
+            return;
+        }
+
+        $request->validate([
+            'data' => 'required|unique:push_subscriptions'
+        ]);
         try {
             PushSubscription::create([
                 'data' => $request->getContent()
             ]);
             return $this->successResponse(null);
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return $this->failsResponse('Failed to subscribe', $e->getMessage());
         }
     }
@@ -30,8 +41,12 @@ class PushSubscriptionController extends Controller
      */
     public function getSubscriptions()
     {
-        $data = PushSubscription::all();
-        return response()->json($data);
+        try {
+            $data = PushSubscription::all();
+            return $this->successResponse($data);
+        } catch (Exception $e) {
+            return $this->failsResponse('Failed to retrieve subscriptions', $e->getMessage());
+        }
     }
 
     /**
@@ -39,20 +54,25 @@ class PushSubscriptionController extends Controller
      */
     public function sendNotification(Request $request, $subscriptionId)
     {
-        $pushSubscription = PushSubscription::find($subscriptionId);
-        $webPush = new WebPush([
-            "VAPID" => [
-                "publicKey" => "BCKU6syjcBdoV3EO3w-k8nior__YBYCTtBPDeD5oqGTwG5D6WVCOdGk2TCOaMQhcJK6-9MvNSAqlb_97CWXshmU",
-                "privateKey" => "ongKJvvPRVBWXF_KFWrPZt4eRaRoV7gXc15kSDwp-sY",
-                "subject" => "http://localhost:5173"
-            ]
-        ]);
-
-        $result = $webPush->sendOneNotification(
-            Subscription::create(json_decode($pushSubscription->data, true)),
-            json_encode($request->input())
-        );
-
-        dd($result);
+        DB::beginTransaction();
+        try {
+            $pushSubscription = PushSubscription::find($subscriptionId);
+            $webPush = new WebPush([
+                "VAPID" => [
+                    "publicKey" => "BCKU6syjcBdoV3EO3w-k8nior__YBYCTtBPDeD5oqGTwG5D6WVCOdGk2TCOaMQhcJK6-9MvNSAqlb_97CWXshmU",
+                    "privateKey" => "ongKJvvPRVBWXF_KFWrPZt4eRaRoV7gXc15kSDwp-sY",
+                    "subject" => "http://localhost:5173"
+                ]
+            ]);
+            $result = $webPush->sendOneNotification(
+                Subscription::create(json_decode($pushSubscription->data, true)),
+                json_encode($request->input())
+            );
+            return $this->successResponse($result);
+            DB::commit();
+        } catch (\Exception $e) {
+            return $this->failsResponse('Failed to send notification', $e->getMessage());
+            DB::rollBack();
+        }
     }
 }
